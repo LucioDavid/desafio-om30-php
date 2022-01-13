1d7459f23d20c78839381720b26e173ee7cddb07
Lúcio Meira David <luciomeiradavid@hotmail.com>
2022-01-13 17:27:19 -0300

    feat(migrations): adicionar controller Migrate

---
+++ application/controllers/Migrate.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migrate extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->library('migration');
+
+        if ($this->migration->latest() === FALSE) {
+            return show_error($this->migration->error_string());
+        }
+
+        echo "Migrou!!";
+    }
+}
