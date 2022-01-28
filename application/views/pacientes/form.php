f0aa3c1b17e6dbf5247e28bf12c057b35c042844
LucioDavid <luciomeiradavid@hotmail.com>
2022-01-28 16:49:59 -0300

    feat(pacientes/form): adicionar botao submit

--- application/views/pacientes/form.php
+++ application/views/pacientes/form.php
@@ -86,5 +86,11 @@
                 </div>
             </div>
         </div>
+
+
+        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+        </button>
+
     </form>
 </div>
